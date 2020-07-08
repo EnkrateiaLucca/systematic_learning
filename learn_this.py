b09afc7efa629589a611841fc37a3cad5086045f
Lucas Nicolosi <lucas.soares@research.fchampalimaud.org>
2020-07-08 17:25:22 +0100

    Added documentation to the set_up_dataset() method

--- learn_this.py
+++ learn_this.py
@@ -11,37 +11,24 @@ import time
 from datetime import datetime
 
 
-# def google_search_show(query, num_links=5):
-#     # Perhaps later will be a return instead of print
-#     useful_results = list(search(query, tld="co.in", num=10, stop=num_links, pause=2))
-#     return useful_results
-
-
-def clip_video(video_path, clipped_output_path):
-    list_clips = literal_eval(input("Give the time stamps for the clips. ([[begin_1,end_1],[begin_2,end_2]]; format of begin and end -> [min,secs])"))
-    clip_video(video_path,clipped_output_path, list_clips)
-
-
-# def youtube_download(vid_url,download_path=r"C:\Users\lucas\Downloads",test=False):
-#     if test:
-#         video_url = 'https://www.youtube.com/watch?v=tXOIvjbNhts' # paste here your Youube videos' url
-#         youtube = pytube.YouTube(video_url)
-#         video = youtube.streams.first()
-#         video.download(download_path)
-#     else:
-#         youtube = pytube.YouTube(vid_url)
-#         video = youtube.streams.first()
-#         video.download(download_path)
 
 def generate_dataset():
     youtube_search = literal_eval(YoutubeSearch(learn_query, max_results=10).to_json())
     results = ["youtube.com" + item["link"] for item in youtube_search["videos"]]
     google_searche_results = google_search_show(learn_query, num_links=10)
 
-    return results
+    return results, google_searche_results
 
 
 def set_up_dataset(csv_file="dataset.csv",text_file='dataset.txt'):
+    """
+    Creates a Dataframe from a text file with urls. 
+    The columns are: 
+    attention_level -> input a number representing how much attention the user will give to that source
+    session_time -> How long it took to finish studying that source
+    date -> The date of this session
+    session_score -> The subjective score the user gives to its own performance on that content.
+    """
     df = pd.read_csv(text_file, sep=" ", header=None)
     df.columns= ["links"]
     cols =  ["attention_level", "session_time","date","session_score"]
@@ -65,25 +52,24 @@ def get_date(save_file=False):
 
 
 if __name__=="__main__":
-    #youtube_download(vid_url)
-    text_file = r"C:\Users\lucas\Desktop\projects\medium_posts\learning_post\dataset.txt"
-    if os.path.isfile(text_file):
-        df = set_up_dataset()
+    text_file = "./dataset.txt" # This is the .txt file with the relevant links (should be adapted to each user).
+    if os.path.isfile(text_file): # Checking if the txt files exists
+        df = set_up_dataset() # If there is a text file it will set up the new dataset as a .csv file
     if not os.path.isdir(".\\sessions"):
-        os.mkdir(".\\sessions")
+        os.mkdir(".\\sessions") # Creates a directory to store the data for each session
     try:
         df = pd.read_csv("dataset.csv")
     except:
         print("Make sure the csv is in the right folder")
     last_index = df["last_index"][0]
-    for i,url in enumerate(df["links"][last_index:]):
-        webbrowser.open(url)
-        start_session = time.time()
+    for i,url in enumerate(df["links"][last_index:]): # Looping over the links
+        webbrowser.open(url) # Opens each link on the default browser 
+        start_session = time.time() 
         date = get_date()
-        df["date"][i] = date
-        att_level = input("Input the attention level for this input (b,s-f,f):")
+        df["date"][i] = date # Stores the date
+        att_level = float(input("Input the attention level for this input (0-10):")) # Requests a score of attention 
         df["attention_level"][i] = att_level
-        next_inp = input("Press enter to go to next link or q to quit training for now")
+        next_inp = input("Press enter to go to next link or q to quit training for now") # To go to the next link
         if next_inp == "q":
             df["last_index"] = i
             end_session = time.time()
@@ -93,16 +79,16 @@ if __name__=="__main__":
             df["session_score"][i] = s_score
             break
         end_session = time.time()
-        s_time = np.round(end_session - start_session,2)
+        s_time = np.round(end_session - start_session,2) # Records the time of that session
         df["session_time"][i] = s_time
-        s_score = float(input("Give a score to your performance in this session (0-10):"))
+        s_score = float(input("Give a score to your performance in this session (0-10):")) # Requests the score for performance
         df["session_score"][i] = s_score
 
     session_df = df.copy()
     date_file = get_date(save_file=True)
-    session_df.to_csv(".\\sessions\\session_{}.csv".format(date_file))
+    session_df.to_csv(".\\sessions\\session_{}.csv".format(date_file)) # Stores the .csv for that session on the sessions directory
 
-    duolingo = input("Do you want to do a quick duolingo session to finish things off? (y/n)")
+    duolingo = input("Do you want to do a quick duolingo session to finish things off? (y/n)") # Finishes prompting the user to do a quick duolingo session to wrap things up  
     if duolingo=="y":
         webbrowser.open("www.duolingo.com")
     
